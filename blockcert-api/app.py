a01e8171ddc710ccb802aa5edbc704efd3eaf638
John Wagenleitner <john.wagenleitner@gmail.com>
2018-10-22 12:56:33 -0700

    show use of DynamoDB from a route

--- blockcert-api/app.py
+++ blockcert-api/app.py
@@ -1,11 +1,18 @@
+import boto3
+
 from chalice import Chalice
 
 app = Chalice(app_name='blockcert-api')
 
+DB = boto3.resource('dynamodb', region_name='us-west-2')
+
+users_table = DB.Table('blockcert.users')
+
 
 @app.route('/users/{user_id}', methods=['GET'])
 def get_users(user_id):
-    return {'hello': f'{user_id}'}
+    response = users_table.get_item(Key={'id': user_id})
+    return response['Item']
 
 
 @app.route('/certs/{cert_id}', methods=['GET'])
