991eb3983555e279746d74ce4cd4bc5d18fab3b6
John Wagenleitner <john.wagenleitner@gmail.com>
2018-11-05 16:03:42 -0800

    wip

--- blockcert-batch/bc-batch-entrypoint.sh
+++ blockcert-batch/bc-batch-entrypoint.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 set -e
 
+echo "$(date): Running entrypoint script"
+
+echo "Arg 1==[${1}]"
+echo "Arg 2==[${2}]"
+echo "Arg 3==[${3}]"
+
 echo "downloading S3 batch directory"
-echo "aws s3 cp --recursive s3://fs.blockcerts.poc/batch/20181024/ /var/blockcerts/"
+echo "aws s3 cp --recursive ${BC_BATCHDIR_S3URL} /var/blockcerts/"
 
 echo "running cert-issuer on the following unsigned certs"
 ls -l /var/blockcerts/unsigned_certificates
 
-cert-issuer -c /var/blockcerts/config/conf.ini
+#cert-issuer -c /var/blockcerts/config/conf.ini
 
 echo "uploading signed blockcerts to S3"
-ls -l /var/blockcerts/blockchain_certificates
-echo "aws s3 cp --recursive /var/blockcerts/blockchain_certificates s3://fs.blockcerts.poc/certs/"
+# ls -l /var/blockcerts/blockchain_certificates
+echo "aws s3 cp --recursive /var/blockcerts/blockchain_certificates ${BC_CERTDIR_S3URL}"
+
+echo "$(date): done"
